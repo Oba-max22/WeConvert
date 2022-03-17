c3045ff0a9aab41b601cb867666db42472e20fac
Emmanuel Macaulay <emmanuelmcl86@gmail.com>
2022-03-17 12:43:41 +0100

    Initial Commit

---
+++ WeConvert/WeConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeConvertApp.swift
+//  WeConvert
+//
+//  Created by Decagon on 17/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
